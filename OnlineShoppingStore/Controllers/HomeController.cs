8573463bd72479217b7fcc054fe4766838a97d89
Admin <Admin@DESKTOP-QI6H2EA>
2023-02-21 09:03:26 +0100

    Add project files.

---
+++ OnlineShoppingStore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbMyOnlineShoppingEntities ctx = new dbMyOnlineShoppingEntities(); 
+        public ActionResult Index(string search,int? page)
+        {
+            HomeIndexViewModel model = new HomeIndexViewModel();
+            return View(model.CreateModel(search, 8, page));
+        }
+
+        public ActionResult AddToCart(int productId)
+        {
+            var cart = new List<Item>();
+            var product = ctx.Tbl_Product.Find(productId);
+            cart.Add(new Item()
+            {
+                Product = product,
+                Quantity = 1
+            });
+            Session["cart"] = cart;
+            return View("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OnlineShoppingStore/Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,35 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Repository;
+using PagedList;
+using PagedList.Mvc;
+using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
+
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+
+        public HomeIndexViewModel CreateModel(string search, int PageSize, int? page)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@search",search??(object)DBNull.Value)
+            };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, PageSize);
+            return new HomeIndexViewModel()
+            {
+                ListOfProducts = data
+            };
+        }
+    }
+}
